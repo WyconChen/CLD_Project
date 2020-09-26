2b71a15fb865fa55c4487b4739098deab3cc9d30
wycon_home <chenweicong1993@126.com>
2020-09-27 00:22:13 +0800

    add webapp

---
+++ Api/common_api/app.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI
+import jinja2
+import os
+try:
+    from Api.Func.mysqlFunc import mysqlFunc
+except:
+    from Api.Func.mysqlFunc import mysqlFunc
+from Api.Func.MysqlModule import MysqlModule
+from Api.Model.model import ProgramModel, Baoyun18Model, Qixin18Model
+
+app = FastAPI()
+
+# templete
+
+# 得到放置模板的目录
+path = '{}/../../webapp/'.format(os.path.dirname(__file__))
+
+# 创建一个加载器, jinja2 会从这个目录中加载模板
+loader = jinja2.FileSystemLoader(path)
+
+# 用加载器创建一个环境, 有了它才能读取模板文件
+env = jinja2.Environment(loader=loader)
+
+# 调用 get_template() 方法加载模板并返回
+template = env.get_template('index.html')
+
+mysqlFunc = mysqlFunc()
+MysqlModule = MysqlModule()
+
+@app.post("/insert/Program")
+async def insertDataToProgram(request_data: ProgramModel):
+    DataDict = {
+    	  "program_id": ProgramModel.program_id,
+    	  "program_name": ProgramModel.program_name
+    }
+    mysqlFunc.insertDataToProgram(DataDict)
+
+@app.post("/insert/Baoyun18")
+async def insertDataToProgram(request_data: Baoyun18Model):
+	# return {"result":"success"}
+    DataDict = {
+    	"program_id" : request_data.program_id,
+    	"temp_id" : request_data.temp_id,
+        "product_id" : request_data.product_id,
+        "product_name": request_data.product_name.encode("utf-8"),
+        "payDesc":request_data.payDesc.encode("utf-8"),
+        "insureDesc":request_data.insureDesc.encode("utf-8"),
+        "first_rate":request_data.first_rate,
+        "second_rate":request_data.second_rate
+    }
+    result = MysqlModule.SaveDataToBaoyun18(DataDict)
+    if result:
+        return {"result":"success"}
+    else:
+        return {"result":"failed"}
+
+@app.get("/getDataFrom/Baoyun18")
+async def getDataFromBaoyun18():
+    result = MysqlModule.GetDataFromBaoyun18()
+    return {"result": result}
+
+@app.post("/insert/Qixin18")
+async def saveDataToQixin18(request_data: Qixin18Model):
+    DataDict = {
+        "program_id": request_data.program_id,
+        "product_id": request_data.product_id,
+        "product_name": request_data.product_name,
+        "plan_Id": request_data.plan_Id,
+        "company_id": request_data.company_id,
+        "company_name": request_data.company_name,
+        "isDetails": request_data.isDetails,
+        "yearPolicyText": request_data.yearPolicyText,
+        "insureAgeText": request_data.insureAgeText,
+        "economyText": request_data.economyText,
+        "feeRateList_1": request_data.feeRateList_1,
+        "feeRateList_2": request_data.feeRateList_2
+    }
+    MysqlModule.SaveDataToQixin18(DataDict)
--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-{'product_id': 104101, 'product_name': '百惠保重大疾病保险', 'plan_Id': 128604, 'company_id': 1105, 'company_name': '百 年人寿', 'isDetails': True, 'yearPolicyText': '次年', 'insureAgeText': '20年,30年缴', 'economyText': '终身 ', 'feeRateList_1': 7.0, 'feeRateList_2': 0.0}
+import os
+
+if __name__ == "__main__":
+    path = os.path.dirname(__file__)
+    print(path+"/./webapp/")

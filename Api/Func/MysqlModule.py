d428d3e64d51e2f38fde521ce170ad45fe85f428
WyconChen <wyconchen@aslcn.com.cn>
2020-10-19 18:27:18 +0800

    add page size

--- Api/Func/MysqlModule.py
+++ Api/Func/MysqlModule.py
@@ -176,7 +176,6 @@ class MysqlModule:
                         count_sql = "SELECT COUNT(DISTINCT(`product_id`)) AS COUNT FROM CLD_Baoyun18 WHERE `product_name` LIKE '%{product_key}%';".format(product_key = datadict["product_key"])
                     cursor.execute(count_sql)
                     count = cursor.fetchone()
-                    print(count)
                     result["total_num"] = int(count[0])
                 return result
             except Exception as e:

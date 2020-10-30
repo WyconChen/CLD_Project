e6a8dc791db29d91b688ad51652416b0fa0d7c07
WyconChen <wyconchen@aslcn.com.cn>
2020-10-30 16:40:23 +0800

    update get function

--- Router/GetDataRouter.py
+++ Router/GetDataRouter.py
@@ -17,7 +17,7 @@ else:
 templates = Jinja2Templates(directory=path)
 
 @GetDataRouter.get("/baoxian/")
-async def App_Run(request:Request, searchType: Optional[int] = 1, page:int = None, program_id:int = None, product_key:str = None, pageSize:int = None):
+async def BaoXianGetData(request:Request, searchType: Optional[int] = 1, page:int = None, program_id:int = None, product_key:str = None, pageSize:int = None):
     datadict = {
         "searchType": 1,
         "page": page or 1,
@@ -29,3 +29,16 @@ async def App_Run(request:Request, searchType: Optional[int] = 1, page:int = Non
     pageList = [datadict["page"]-2, datadict["page"]-1, datadict["page"], datadict["page"]+1, datadict["page"]+2]
     return templates.TemplateResponse("index.html", {"request":request,"ProductsList": product_list, "DataDict":datadict, "pageList":pageList, "total_num":total_num})
 
+@GetDataRouter.get("/baoxian/test")
+async def BaoXianGetData(request:Request, searchType: Optional[int] = 1, page:int = None, program_id:int = None, product_key:str = None, pageSize:int = None):
+    datadict = {
+        "searchType": 1,
+        "page": page or 1,
+        "program_id": program_id or 999,
+        "product_key": product_key or "",
+        "pageSize": pageSize or 5
+    }
+    product_list, total_num = DBHandler.GetJsonDataFromDB(datadict)
+    pageList = [datadict["page"]-2, datadict["page"]-1, datadict["page"], datadict["page"]+1, datadict["page"]+2]
+    # return templates.TemplateResponse("index.html", {"request":request,"ProductsList": product_list, "DataDict":datadict, "pageList":pageList, "total_num":total_num})
+    return product_list

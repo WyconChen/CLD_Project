9fb78f345aa70df87417cab513ee3ae1181d6714
WyconChen <wyconchen@aslcn.com.cn>
2020-11-24 17:41:33 +0800

    update fontend

--- app_test.py
+++ app_test.py
@@ -17,4 +17,4 @@ app.mount("/static", StaticFiles(directory=path), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app=app, host="0.0.0.0", port=8002, reload = True)
+    uvicorn.run(app=app, host="0.0.0.0", port=8002)

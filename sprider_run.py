af4c02bd4cd0e65e6ac26222f89cec0032cab97a
WyconChen <wyconchen@aslcn.com.cn>
2021-03-12 09:49:34 +0800

    update niubao template

--- sprider_run.py
+++ sprider_run.py
@@ -22,14 +22,14 @@ if __name__ == "__main__":
         BaoYun18_Thread = threading.Thread(target=BaoYun18.run)
         FengQi_Thread = threading.Thread(target=FengQi.run)
         Niubao100_Thread = threading.Thread(target=Niubao100.run)
-        QiXin18_Thread = threading.Thread(target=QiXin18.run)
+        #QiXin18_Thread = threading.Thread(target=QiXin18.run)
         Yisheng_Thread = threading.Thread(target=Yisheng.run)
         ZhongBao_Thread = threading.Thread(target=ZhongBao.run)
 
         BaoYun18_Thread.start()
         FengQi_Thread.start()
         Niubao100_Thread.start()
-        QiXin18_Thread.start()
+        #QiXin18_Thread.start()
         Yisheng_Thread.start()
         ZhongBao_Thread.start()
     else:
